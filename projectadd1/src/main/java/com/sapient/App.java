09459ac5afd52d0cde51f370b3f68e1d7576d755
Kanika Kadian <kanikakadian19@gmail.com>
2021-06-21 20:46:19 +0530

    my commit

---
+++ projectadd1/src/main/java/com/sapient/App.java
@@ -0,0 +1,18 @@
+package com.sapient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println(new App().add(2,3));
+    }
+	
+     public int add(int a, int b){
+	return a+b;
+	}
+}
